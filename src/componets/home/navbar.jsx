d371d8ab8f8a007a9a255279bcb2b4676c1b729b
BSC-04-20 <bsc-04-20@unima.ac.mw>
2025-01-03 04:39:49 -0800

    Responsive navbar

--- src/componets/home/navbar.jsx
+++ src/componets/home/navbar.jsx
@@ -1,9 +1,11 @@
+import { UilBars } from "@iconscout/react-unicons"
 
 export default function NavBar(){
     return(
         <>
-            <div className="lg:h-24 lg:bg-black lg:bg-opacity-85">
-                <div className="flex lg:flex-row lg:items-center lg:justify-end lg:space-x-5 lg:text-lg lg:text-white lg:h-full lg:mr-10">
+            <div className="bg-black h-10 lg:h-10 lg:bg-black lg:bg-opacity-85">
+                <UilBars className="text-white ml-auto scale-150 lg:hidden"/>
+                <div className="hidden lg:flex lg:flex-row lg:items-center lg:justify-end lg:space-x-5 lg:text-lg lg:text-white lg:h-full lg:mr-10">
                         <span>Home</span>
                         <span>Latest</span>
                         <span>Popular</span>
@@ -13,3 +15,4 @@ export default function NavBar(){
         </>
     )
 }
+
