fe03f80280cc55bf3ab1975dab3330810fcdea5b
BSC-04-20 <bsc-04-20@unima.ac.mw>
2025-01-05 05:24:25 -0800

    Repositioned the cartoons background position

--- src/pages/cartoons.jsx
+++ src/pages/cartoons.jsx
@@ -5,7 +5,7 @@ import KungfuPanda from "../../public/kungfu_panda.jpg"
 export default function Cartoons(){
     return(
         <>
-             <div className=" bg-black h-[100vh] bg-no-repeat bg-cover" style={{backgroundImage: `url(${KungfuPanda}`}}>
+             <div className=" bg-black h-[100vh] bg-no-repeat bg-cover" style={{backgroundImage: `url(${KungfuPanda}`, backgroundPosition:'50% 0%'}}>
                 <NavBar />
                 <HerosSearchBar/>
              </div>
