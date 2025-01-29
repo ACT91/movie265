edf7803387b8db35943b12d06bb3a75a97827ffd
Benjamin Chilangwe <bsc-com-19-20@unima.ac.mw>
2025-01-29 11:24:38 +0000

    fetchAPI

---
+++ src/config/urlhorror.jsx
@@ -0,0 +1,10 @@
+// import axios from "axios";
+
+// const urlHorror = axios.create({
+//     baseURL:"https://api.themoviedb.org/3/movie",
+//     headers:{
+//         Authorization: `Bear eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MzMyNWNmNjZiNWQzMWNhYmQ0MDcyMGQ1MGIzNDkwMCIsIm5iZiI6MTczODA1MDM0Mi45NjUsInN1YiI6IjY3OTg4YjI2NWRjOGIzYjcyZDJhZjM2NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Yuu265z8inF70mZbJivWleXuMzVffcMjoFfyOCPUKQs`
+//     }
+// })
+
+// export default urlHorror;
--- src/pages/horror.jsx
+++ src/pages/horror.jsx
@@ -1,14 +1,47 @@
 import NavBar from "../componets/home/navbar";
 import HerosSearchBar from "../componets/home/SearchBar";
 import HorrorPic from "../../public/conjuring.png"
+// import { useEffect, useState } from "react";
+// import urlHorror from "../config/urlhorror";
 
 export default function Horror(){
+    // const [results, setResults] = useState([])
+    // const fetch = async () => {
+    //     try{
+    //         const res = await urlHorror("/popular")
+    //         console.log(res.page.results)
+
+    //         setResults(res.page.results)
+    //     }
+    //     catch(error){
+    //         console.log(error)
+    //     }
+    // }
+
+
+    // useEffect(()=>{
+    //     fetch()
+    // }, [])
     return(
-        <>
-             <div className=" bg-black h-[100vh] bg-no-repeat bg-cover" style={{backgroundImage: `url(${HorrorPic}`, backgroundPosition:'50% 0%'}}>
-                <NavBar />
-                <HerosSearchBar/>
-             </div>
-        </>
-    )
+        <div>
+            <>
+                <div className=" bg-black h-[100vh] bg-no-repeat bg-cover" style={{backgroundImage: `url(${HorrorPic}`, backgroundPosition:'50% 0%'}}>
+                    <NavBar />
+                    <HerosSearchBar/>
+                </div>
+            </>
+
+            {/* <div>
+                {
+                    results.map((movie) => (
+                        <div key={movie.id}>
+                            <h2>{movie.title}</h2>
+                            <img src={movie.poster_path}/>
+                        </div>
+                    ) )
+                }
+             </div> */}
+
+        </div>
+    );
 }
